b5270a4652ceefc65cec59bd357bd9c7ce46f651
Hanabata <93629352+hanabata84@users.noreply.github.com>
2022-12-05 19:45:53 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"];
+
+let passwordOne = document.getElementById("password-1")
+let passwordTwo = document.getElementById("password-2")
+
+
+function generateRandomPassword() {
+    clearText()
+    for (let i = 0; i < 15; i++) {
+        passwordOne.textContent += characters[generateRandomNo()]
+        passwordTwo.textContent += characters[generateRandomNo()]
+    }
+}
+
+function generateRandomNo() {
+    return Math.floor(Math.random() * characters.length)
+}
+
+function clearText() {
+    if (passwordOne.textContent !== "" && passwordTwo !== "") {
+        passwordOne.textContent = ""
+        passwordTwo.textContent = ""
+    }
+}
